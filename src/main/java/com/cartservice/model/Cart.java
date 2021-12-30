cd9eeff3a50697cce4c0dada3601d9ae5a4407d4
jewelcsebu <jewelchowdhurybucse@gmail.com>
2021-12-30 18:15:43 +0600

    cart service init

---
+++ src/main/java/com/cartservice/model/Cart.java
@@ -0,0 +1,25 @@
+package com.cartservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import javax.persistence.*;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long customerId;
+
+}
